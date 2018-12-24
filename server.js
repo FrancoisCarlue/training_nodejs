b7321ed8e0d2d1b77f998b5edda4b2144b436ddc
Francois Carlue <francois.carlue@centrale-marseille.fr>
2018-12-24 10:45:46 +0100

    installation of express-handlebars for templating

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ Promise = require('bluebird');
 mongoose = Promise.promisifyAll(require('mongoose'));
 fs = Promise.promisifyAll(require('fs'));
 logLib = require('./lib/log'); //bien partir de la racine pour le require de notre lib maison
+exphbs = require('express-handlebars');
+
 app = express(); // mis en global pour que ce soit accessible aux autres fichiers de routing
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
